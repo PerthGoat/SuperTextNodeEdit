6b49c9983e205ff592f0d92018679753a4f0377e
PerthGoat <administrator@ryancdeyong.us>
2021-11-14 08:26:48 -0800

    basic program layout

---
+++ SuperText.py
@@ -0,0 +1,20 @@
+# GUI utilities
+# Tkinter
+# Chosen because Tkinter is shipped standard with Python and does not require GTK
+# or anything complex to get it running
+
+import tkinter as tk
+from tkinter import messagebox, font, ttk
+
+# IO utilities
+# these handle parsing, renaming, removing, and moving the various node/file trees
+import io
+import os
+import glob
+import shutil
+
+# multithreading used to open multiple windows and allow for popup dialogs for renaming
+from threading import Thread
+
+# PIL functions used for grabbing the clipboard in a cross-platform way
+from PIL import Image, ImageTK, ImageGrab
