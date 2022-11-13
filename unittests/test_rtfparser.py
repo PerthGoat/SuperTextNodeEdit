eb115968407c629c0a1b866161573b66163ca267
PerthGoat <administrator@ryancdeyong.us>
2022-11-13 14:08:57 -0800

    Addd basic unit tests to RTF parser

---
+++ unittests/test_rtfparser.py
@@ -0,0 +1,19 @@
+# RTF parsing
+from src.RTFParser import RTFParser
+
+import unittest
+
+class TestRTFParsing(unittest.TestCase):
+    def test_basicfile(self):
+        testfile_path = 'unittests/files/t2.rtf'
+        with open(testfile_path, 'r') as fi:
+            rtf_text = fi.read()
+
+        RTFParser(rtf_text).parse()
+
+    def test_parsefail(self):
+        badfile_path = 'unittests/files/badfile.rtf'
+        with open(badfile_path, 'r') as fi:
+            rtf_text = fi.read()
+
+        RTFParser(rtf_text).parse()
